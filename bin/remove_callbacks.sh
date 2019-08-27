4a6f777a6fda4cf53c0106235de9e8da0816aa4b
Christian Fenzl <fenzl@t-r-t.at>
2019-08-27 11:51:08 +0200

    remove_callbacks: No hardcoded path, fixes #26

--- bin/remove_callbacks.sh
+++ bin/remove_callbacks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-perl /opt/loxberry/webfrontend/htmlauth/plugins/nukismartlock/index.cgi ajax=callback_remove_all_from_all
+perl REPLACELBPHTMLAUTHDIR/index.cgi ajax=callback_remove_all_from_all
